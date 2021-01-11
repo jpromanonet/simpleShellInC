f54028142d839e17a9b4348986cd4999607d0b31
Juan P. Romano <programming@jpromano.net>
2021-01-11 16:30:45 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+/***************************************************************************//**
+  @file         main.c
+  @author       Juan P. Romano
+  @date         Monday, 11 January, 2021
+  @brief        SSIC (Simple Shell in C)
+*******************************************************************************/
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
