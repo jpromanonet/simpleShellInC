6aa702a44049885e50e7cf60be1b190d7d905748
Juan P. Romano <programming@jpromano.net>
2021-01-13 13:00:36 -0300

    Launch function

--- main.c
+++ main.c
@@ -90,3 +90,31 @@ int ssc_exit(char **args){
     printf("I will be back user!");
     return 0;
 }
+
+/**
+  @brief Launch a program and wait for it to terminate.
+  @param args Null terminated list of arguments (including program).
+  @return Always returns 1, to continue execution.
+ */
+
+int ssc_launch(char **args){
+    pid_t pid;
+    int status;
+
+    pid = fork();
+    if(pid == 0){
+        // Child process
+        if(execvp(args[0], args) == -1){
+            perror("ssc");
+        }
+        exit(EXIT_FAILURE);
+    } else if(pid < 0){
+        // Error forking
+        perror("ssc");
+    } else {
+        // Parent process
+        do {
+            waitpid(pid, status, WUNTRACED);
+        }
+    }
+}
